7f4e82204170adbd7b125f33b09bd16d609c2bb2
ratimiranda <116958116+ratimiranda@users.noreply.github.com>
2022-10-29 16:25:25 +0200

    Add grocery list endpoint

--- src/main/java/com/example/demo/RestService.java
+++ src/main/java/com/example/demo/RestService.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.model.GroceryList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +15,11 @@ public class RestService {
 		SpringApplication.run(RestService.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@GetMapping("/groceries/{name}")
+	public GroceryList getGroceries(@PathVariable String name) {
+		GroceryList groceryList = new GroceryList();
+		groceryList.setTitle(name);
+		return groceryList;
 	}
 
 }
---
+++ src/main/java/com/example/demo/model/GroceryList.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroceryList {
+
+    private String title;
+    private List<String> list = new ArrayList<>();
+
+    public void addItem(String item) {
+        list.add(item);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
